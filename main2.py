6f3da4f12f87b633a388568a742a9dbfc71f6460
ukulele <mateuszkukiela@gmail.com>
2021-06-29 22:38:20 +0200

    Test

--- main2.py
+++ main2.py
@@ -130,13 +130,12 @@ def main_menu():
     while True:
         if my_rotary.counter >= len(options) - 1:
             my_rotary.counter = len(options) - 1
+        global action
         action = options[my_rotary.counter]['action']
         lcd_send_byte(LCD_LINE_1, LCD_CMD)
         lcd_message(str(options[my_rotary.counter]['name']).upper())
         lcd_send_byte(LCD_LINE_2, LCD_CMD)
         lcd_message('')
-        global action
-        action = options[my_rotary.counter]['action']
         time.sleep(.001)
 
 
